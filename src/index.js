d92be7b3a1d3ac1ba05a383770c8711bf320f6ca
Josemaria Links <josemaria.links2@gmail.com>
2023-05-16 19:53:59 +0200

    feat: add sass; rearrange files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Error from "./pages/Error";
+import "./index.scss";
+import Home from "./component/Page_home/Home";
+import About from "./component/Page_about/About";
+import Error from "./component/Page_error/Error";
+import Header from "./component/common/Header";
+import Footer from "./component/common/Footer";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -11,11 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>
 );
